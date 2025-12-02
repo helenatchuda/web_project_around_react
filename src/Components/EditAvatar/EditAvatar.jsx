d40db4fad43ac3af49a9e7d59da0467a9ae77481
Helena Silva <helenatchuda@gmail.com>
2025-12-02 21:01:10 +0000

    add imagepopup

--- src/Components/EditAvatar/EditAvatar.jsx
+++ src/Components/EditAvatar/EditAvatar.jsx
@@ -1,13 +1,26 @@
 export default function EditAvatar() {
   return (
     <>
-      <button className="profile__edit-button" aria-label="Editar perfil">
-        <img
-          src="./images/Vector (2).png"
-          alt="Ícone editar perfil"
-          className=""
+     <form className="form" name="photo-form" id="new-photo-form" noValidate>
+      <label className="form__field">
+        <input
+          className="form__input form__input_type_photo-name"
+          id="Photo-name"
+          maxLength="30"
+          minLength="1"
+          name="Photo-name"
+          placeholder="imgagelink"
+          required
+          type="url"
         />
+        <span className="form__error" id="photo-name-error"></span>
+      </label>
+     
+
+      <button className=" popup__save-button popup__save-button_disabled">
+        Salvar
       </button>
+    </form>
     </>
   );
 }
--- src/Components/ImagePopup/ImagePopup.jsx
+++ src/Components/ImagePopup/ImagePopup.jsx
@@ -1,8 +1,28 @@
-export default function ImagePopup(){
+import React, { useState } from 'react';
 
+export default function ImagePopup(props){
+
+      
     return(
-        <>
-        </>
+   
+      <div class="popup__container-image">
+        <img
+          src="./images/Close Icon.png"
+          alt="Fechar imagem"
+          class="popup__close"
+        />
+
+
+        <img src={props.link}
+
+          alt="Imagem ampliada"
+          class="popup__image"
+        />
+        <p class="popup__image-title">{props.name}</p>
+      </div>
+  
+      
+      
 
     )
 }
